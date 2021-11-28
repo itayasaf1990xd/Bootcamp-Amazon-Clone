f3dff54a0e3bad77d9e06a3717e3998781c8ce1c
itayasaf1990xd <89840492+itayasaf1990xd@users.noreply.github.com>
2021-11-28 05:07:32 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,53 @@
 function addToCart() {
   document.querySelector(".cart-count-count").textContent = 1;
 }
+
+document
+  .querySelector(".alt-images-item1")
+  .addEventListener("mouseenter", function (event) {
+    document.querySelector(".main-image-image").src =
+      "../assets/images/71fnafSnc9S._AC_SL1500_.jpg";
+  });
+
+
+document
+  .querySelector(".alt-images-item2")
+  .addEventListener("mouseenter", function (event) {
+    document.querySelector(".main-image-image").src =
+      "../assets/images/61sLH7yD5nS._AC_SX679_.jpg";
+  });
+
+  document
+  .querySelector(".alt-images-item3")
+  .addEventListener("mouseenter", function (event) {
+    document.querySelector(".main-image-image").src =
+      "../assets/images/6149+8EzgIS._AC_SX679_.jpg";
+  });
+
+  document
+  .querySelector(".alt-images-item4")
+  .addEventListener("mouseenter", function (event) {
+    document.querySelector(".main-image-image").src =
+      "../assets/images/71ZuBTH7D3S._AC_SY450_.jpg";
+  });
+
+  document
+  .querySelector(".alt-images-item5")
+  .addEventListener("mouseenter", function (event) {
+    document.querySelector(".main-image-image").src =
+      "../assets/images/71We1421U7S._AC_SX679_.jpg";
+  });
+
+  document
+  .querySelector(".alt-images-item6")
+  .addEventListener("mouseenter", function (event) {
+    document.querySelector(".main-image-image").src =
+      "../assets/images/61khEKu0HhS._AC_SX679_.jpg";
+  });
+
+  document
+  .querySelector(".alt-images-item7")
+  .addEventListener("mouseenter", function (event) {
+    document.querySelector(".main-image-image").src =
+      "../assets/images/asd1e213r666.png";
+  });
